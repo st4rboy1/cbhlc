42149afc513e04f82f4d34856277e70113462a8d
st4rboy1 <st4rboy1@gmail.com>
2025-09-08 14:18:49 +0800

    remove unused imports

--- resources/js/pages/about.tsx
+++ resources/js/pages/about.tsx
@@ -1,6 +1,5 @@
-import { dashboard, login, register } from '@/routes';
 import { type SharedData } from '@/types';
-import { Head, Link, usePage } from '@inertiajs/react';
+import { Head, usePage } from '@inertiajs/react';
 
 export default function Welcome() {
     const { auth } = usePage<SharedData>().props;
