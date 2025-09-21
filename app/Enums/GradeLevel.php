baf5f4ee9c715da822849edb69b6fba6c40f06df
st4rboy1 <cyphroks@gmail.com>
2025-09-22 02:32:40 +0800

    refactor: update GradeLevel enum to support only K-6 grades
    - Remove grades 7-12 as school only caters to elementary (K-6)
    - Simplify to single Kinder level instead of Kinder 1 & 2
    - Remove Nursery level as not used by this school
    - Update helper methods for K-6 only
    - Add getNumericLevel() method for grade ordering
    - Remove isHighSchool() method as not applicable

--- app/Enums/GradeLevel.php
+++ app/Enums/GradeLevel.php
@@ -4,21 +4,13 @@ namespace App\Enums;
 
 enum GradeLevel: string
 {
-    case NURSERY = 'Nursery';
-    case KINDER_1 = 'Kinder 1';
-    case KINDER_2 = 'Kinder 2';
+    case KINDER = 'Kinder';
     case GRADE_1 = 'Grade 1';
     case GRADE_2 = 'Grade 2';
     case GRADE_3 = 'Grade 3';
     case GRADE_4 = 'Grade 4';
     case GRADE_5 = 'Grade 5';
     case GRADE_6 = 'Grade 6';
-    case GRADE_7 = 'Grade 7';
-    case GRADE_8 = 'Grade 8';
-    case GRADE_9 = 'Grade 9';
-    case GRADE_10 = 'Grade 10';
-    case GRADE_11 = 'Grade 11';
-    case GRADE_12 = 'Grade 12';
 
     public static function values(): array
     {
@@ -30,30 +22,35 @@ enum GradeLevel: string
         return $this->value;
     }
 
+    /**
+     * Check if grade level is elementary (all levels are elementary for this school)
+     */
     public function isElementary(): bool
     {
-        return in_array($this, [
-            self::NURSERY,
-            self::KINDER_1,
-            self::KINDER_2,
-            self::GRADE_1,
-            self::GRADE_2,
-            self::GRADE_3,
-            self::GRADE_4,
-            self::GRADE_5,
-            self::GRADE_6,
-        ]);
+        return true; // All grade levels in this school are elementary (K-6)
     }
 
-    public function isHighSchool(): bool
+    /**
+     * Check if grade level is kindergarten
+     */
+    public function isKindergarten(): bool
     {
-        return in_array($this, [
-            self::GRADE_7,
-            self::GRADE_8,
-            self::GRADE_9,
-            self::GRADE_10,
-            self::GRADE_11,
-            self::GRADE_12,
-        ]);
+        return $this === self::KINDER;
+    }
+
+    /**
+     * Get the numeric grade level (0 for Kinder, 1-6 for grades)
+     */
+    public function getNumericLevel(): int
+    {
+        return match ($this) {
+            self::KINDER => 0,
+            self::GRADE_1 => 1,
+            self::GRADE_2 => 2,
+            self::GRADE_3 => 3,
+            self::GRADE_4 => 4,
+            self::GRADE_5 => 5,
+            self::GRADE_6 => 6,
+        };
     }
 }
