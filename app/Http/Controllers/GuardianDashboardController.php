4c9297876f0d6a57f0af57eb1aeb0068f33b4219
st4rboy1 <cyphroks@gmail.com>
2025-09-22 11:17:41 +0800

    style: fix code formatting in GuardianDashboardController

--- app/Http/Controllers/GuardianDashboardController.php
+++ app/Http/Controllers/GuardianDashboardController.php
@@ -31,7 +31,7 @@ class GuardianDashboardController extends Controller
 
                     return [
                         'id' => $student->id,
-                        'name' => trim($student->first_name . ' ' . $student->last_name),
+                        'name' => trim($student->first_name.' '.$student->last_name),
                         'grade' => $student->grade_level?->value ?? 'Not Enrolled',
                         'enrollmentStatus' => $latestEnrollment
                             ? ucfirst($latestEnrollment->enrollment_status->value)
