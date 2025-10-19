413f396f6bac4d6566860ea94e321cb586c1400b
Mike Castro <mkcastro24@gmail.com>
2025-10-19 18:39:30 +0800

    feat: Add deployment script and Nginx configuration for cbhlc.com

---
+++ deployment-script.sh
@@ -0,0 +1,20 @@
+cd /home/forge/cbhlc.com
+
+$FORGE_PHP artisan down
+
+git pull origin $FORGE_SITE_BRANCH
+$FORGE_COMPOSER install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Prevent concurrent php-fpm reloads...
+touch /tmp/fpmlock 2>/dev/null || true
+( flock -w 10 9 || exit 1
+echo 'Reloading PHP FPM...'; sudo -S service $FORGE_PHP_FPM reload ) 9</tmp/fpmlock
+
+npm ci && npm run build
+
+if [ -f artisan ]; then
+    $FORGE_PHP artisan optimize
+    $FORGE_PHP artisan migrate --force
+fi
+
+$FORGE_PHP artisan up
