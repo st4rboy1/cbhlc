67cae66bbf63541213a310ccaef5f9d131d0adf3
st4rboy1 <cyphroks@gmail.com>
2025-11-01 18:47:25 +0800

    Fix #478: Create system_settings and setting_history migrations (#542)
    - Rename settings table to system_settings
    - Add type, description, is_public fields to system_settings
    - Create setting_history table for audit trail
    - Update Setting model to reference new table name
    - Update seeder with core settings (school_name, timezone, items_per_page)
    - Add metadata (type, description, is_public) to all seeded settings
    
    Refs #478

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'system_settings';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
@@ -14,5 +21,17 @@ class Setting extends Model
     protected $fillable = [
         'key',
         'value',
+        'type',
+        'description',
+        'is_public',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'is_public' => 'boolean',
     ];
 }
